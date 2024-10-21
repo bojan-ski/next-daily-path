ff7a8b508d34c29feaaa06be024f441f56e221b8
Bojan SKI <codeucenje@gmail.com>
2024-10-21 16:53:42 +0200

    footer component minor fix

--- components/appLayout/footer/Footer.jsx
+++ components/appLayout/footer/Footer.jsx
@@ -2,11 +2,11 @@
 import AppDeveloperRights from "./AppDeveloperRights"
 import FooterNavbar from "./FooterNavbar"
 
-const Footer = () => {   
+const Footer = () => {
     return (
         <footer className="text-center bg-orange-300 py-4">
-            <FooterNavbar/>
-            <AppDeveloperRights/>
+            <FooterNavbar />
+            <AppDeveloperRights />
         </footer>
     )
 }
